0b88bbe6daf12bd7031b3ed50ebc5eac74012459
Andy Chrismer <andy.dev.91@outlook.com>
2023-11-15 13:35:30 -0600

    complete conditions.js assignment, made corrections to error in my code.

---
+++ js/conditionals.js
@@ -0,0 +1,204 @@
+"use strict";
+console.log("Inside conditions.js")
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Create a function named `analyzeColor` that accepts a string that is a color
+ * name as input. This function should return a message which relates to the
+ * color stated in the argument of the function. For colors you do not have
+ * responses written for, return a string stating so
+ *
+ * Example:
+ *  > analyzeColor('blue') // returns "blue is the color of the sky"
+ *  > analyzeColor('red') // returns "Strawberries are red"
+ *
+ *
+ *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+ *
+ * You should use an if-else-if-else block to return different messages.
+ *
+ * Test your function by passing various string literals to it and
+ * console.logging the function's return value
+ */
+// function analyzeColor(color) {
+//     if (color === 'blue') {
+//         return "blue is the color of the sky";
+//     } else if (color === 'red') {
+//         return "strawberries are red";
+//     } else {
+//         return `I dont't know anything about ${color}`
+//     }
+// }
+console.log(analyzeColor(`blue`))
+console.log(analyzeColor(`red`))
+console.log(analyzeColor(`green`))
+console.log(analyzeColor(`cyan`))
+
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color every time the page loads)
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+/**
+ * TODO:
+ * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
+ * You should see a different message every time you refresh the page
+ */
+// console.log(analyzeColor(randomColor));
+
+/**
+ * TODO:
+ * Comment out the code above, and refactor your function to use a switch-case statement
+ */
+function analyzeColor(color) {
+    let returnMessage;
+    switch (color) {
+        case 'blue':
+            returnMessage = 'blue is the color of the sky';
+            break;
+        case 'red':
+            returnMessage = 'Strawberries are red';
+            break;
+        default:
+            returnMessage = `I don't know anything about ${color}`;
+            break;
+    }
+    return returnMessage
+}
+/**
+ * TODO:
+ * Prompt the user for a color when the page loads, and pass the input from the
+ * user to your `analyzeColor` function. Alert the return value from your
+ * function to show it to the user.
+ */
+// const userColor = prompt("Enter a color");
+// const colorMessage = analyzeColor(userColor;);
+// alert(colorMessage);
+
+// alert(analyzeColor(prompt("Enter a color")))
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Suppose there's a promotion in Walmart, each customer is given a randomly
+ * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+ * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+ * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+ * everything for free!.
+ *
+ * Write a function named `calculateTotal` which accepts a lucky number and total
+ * amount, and returns the discounted price.
+ *
+ * Example:
+ * calculateTotal(0, 100) // returns 100
+ * calculateTotal(4, 100) // returns 50
+ * calculateTotal(5, 100) // returns 0
+ *
+ * Test your function by passing it various values and checking for the expected
+ * return value.
+ */
+function calculateTotal(luckyNumber, totalAmount) {
+    if(luckyNumber === 0) {
+        return totalAmount
+    } else if(luckyNumber === 5) {
+        return 0;
+    } else if (luckyNumber === 1) {
+        return totalAmount * 0.9;
+    } else if (luckyNumber === 2) {
+        return totalAmount * 0.75;
+    } else if(luckyNumber === 3) {
+        return totalAmount *0.65;
+    } else if (luckyNumber !== 4) {
+    } else {
+        {
+            return 'incorrect amount'
+        }
+    }
+}
+
+console.log(calculateTotal(1, 100));
+console.log(calculateTotal(5, 123456789));
+console.log(calculateTotal(3, 200));
+/**
+ * TODO:
+ * Uncomment the line below to generate a random number between 0 and 5.
+ * (In this line of code, 0 is inclusive, and 6 is exclusive)
+ * Prompt the user for their total bill, then use your `calculateTotal` function
+ * and alerts to display to the user what their lucky number was, what their
+ * price before the discount was, and what their price after the discount is.
+ */
+// Generate a random number between 0 and 6
+ const luckyNumber = Math.floor(Math.random() * 6);
+const userTotal = prompt("enter your total amount");
+const totalAfterDiscount = calculateTotal(luckyNumber, userTotal);
+alert(`Your lucky number is: ${luckyNumber}`);
+alert(`The price before discount is: $${userTotal}`);
+alert(`The price after discount is: $${totalAfterDiscount}`)
+/**
+ * TODO:
+ * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+ * would like to enter a number. If they click 'Ok', prompt the user for a
+ * number, then use 3 separate alerts to tell the user:
+ *
+ * - whether the number is even or odd
+ * - what the number plus 100 is
+ * - if the number is negative or positive
+ *
+ * Do *NOT* display any of the above information
+ * if the user enters a value that is not of the number data type.
+ * Instead, use an alert to inform them of the incorrect input data type.
+ *
+ *
+ * Can you refactor your code to use functions?
+ * HINT: The way we prompt for a value could be improved
+ */
+
+function alertEvenOrOdd(number) {
+    // -------------- Even or Odd -----------------
+    // These two lines are the same as the next 11
+    const isEven = number % 2 === 0;
+    alert(`${number} is ${isEven ? 'even' : 'odd'}`);
+
+    // let isEven;
+    // if(userNumber % 2 === 0) {
+    //     isEven = true;
+    // } else {
+    //     isEven = false;
+    // }
+    // if(isEven) {
+    //     alert(`${userNumber} is even`);
+    // } else {
+    //     alert(`${userNumber} is odd`);
+    // }
+}
+
+function alertNumPlus100(number) {
+    // -------------- Number plus 100 -----------------
+    const numPlus100 = number + 100;
+    alert(`${number} + 100 = ${numPlus100}`);
+}
+
+function alertPositiveOrNegative(number) {
+    // -------------- Positive or Negative -----------------
+    const isPositive = number >= 0;
+    alert(`${number} is ${isPositive ? 'positive' : 'negative'}`);
+}
+
+
+
+const userContinue = confirm("Do you want to enter a number?");
+
+if(userContinue) {
+    const userNumber = Number(prompt("Enter a number"));
+    if(!isNaN(userNumber)) {
+        alertEvenOrOdd(userNumber);
+        alertNumPlus100(userNumber);
+        alertPositiveOrNegative(userNumber);
+    } else {
+        alert("You didn't enter a valid number");
+    }
+}
